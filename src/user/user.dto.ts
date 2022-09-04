7b7a0105d75ab610220e38cdc561144d1b56ffb1
rencarJJM <loters@imsmobility.co.kr>
2022-09-04 23:45:31 +0900

    🚧  CRUD 작업 중

---
+++ src/user/user.dto.ts
@@ -0,0 +1,17 @@
+export class ReqUserDataDto {
+  id: string;
+  email: string;
+  username: string;
+}
+
+// export class ReqCreateUserDto {
+//   id: string;
+//   email: string;
+//   username: string;
+// }
+
+export class ResUserDataDto {
+  id: string;
+  email: string;
+  username: string;
+}
